02f86f76c8e4ffd936a968ea4eb26a61939e393d
hboisgibault <h.boisgibault@gmail.com>
2021-02-08 17:34:11 +0100

    Extracting module

--- logora/src/androidTest/java/com/logora/logora_android/ExampleInstrumentedTest.java
+++ logora/src/androidTest/java/com/logora/logora_android/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.applicationpoc", appContext.getPackageName());
+        assertEquals("com.logora.logora_android.test", appContext.getPackageName());
     }
 }
