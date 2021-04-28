78aed4b9ef13961d630bc75087e917b342c175f2
hboisgibault <h.boisgibault@gmail.com>
2021-04-28 16:44:32 +0200

    Cleaning up

--- logora/src/main/java/com/logora/logora_android/views/VoteBoxView.java
+++ logora/src/main/java/com/logora/logora_android/views/VoteBoxView.java
@@ -5,8 +5,6 @@ import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.Resources;
 import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
-import android.graphics.drawable.LayerDrawable;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.ViewTreeObserver;
